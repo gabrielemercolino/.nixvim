a5287614fcd1bb5c95f957af672d6a5d0df9fb39
gabrielemercolino <gmercolino2003@gmail.com>
2024-03-13 22:15:18 +0100

    feat: adding new config

--- config/default.nix
+++ config/default.nix
@@ -7,9 +7,15 @@
     ./colorscheme/colorscheme.nix
     ./plugins/ui/statusline.nix
     ./plugins/ui/nvimtree.nix
+    ./plugins/ui/bufferline.nix
     
     # LSP
     ./plugins/lsp/lsp.nix
+    # ./plugins/lsp/extra.nix
+    
+    # Completitions
+    ./plugins/completitions/cmp.nix
+    ./plugins/completitions/luasnip.nix
 
     # Utils
     ./plugins/utils/telescope.nix
