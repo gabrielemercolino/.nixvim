4d1cce33e870954f21c88c69c966289acdad08bb
gabrielemercolino <gmercolino2003@gmail.com>
2024-03-14 08:30:49 +0100

    feat: feeling more like catppuccin now

--- config/colorscheme/colorscheme.nix
+++ config/colorscheme/colorscheme.nix
@@ -1,5 +1,33 @@
 {
   colorschemes.gruvbox = {
+    enable = false;
+  };
+
+  colorschemes.dracula = {
+    enable = false;
+  };
+  
+  colorschemes.tokyonight = {
+    enable = false;
+  };
+  
+  colorschemes.nord = {
+    enable = false;
+  };
+
+  colorschemes.onedark = {
+    enable = false;
+  };
+
+  colorschemes.one = {
+    enable = false;
+  };
+  
+  colorschemes.catppuccin = {
     enable = true;
+  };
+
+  colorschemes.oxocarbon = {
+    enable = false;
   };
 }
