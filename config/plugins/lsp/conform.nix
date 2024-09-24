00cb8064e6925de06a9202532a064416a4dc2606
Gabriele <gmercolino2003@gmail.com>
2024-09-24 09:41:22 +0200

    refactor: updated deprecated settings with new syntax

--- config/plugins/lsp/conform.nix
+++ config/plugins/lsp/conform.nix
@@ -1,29 +1,25 @@
 { lib, pkgs, ... }: {
   plugins.conform-nvim = {
     enable = true;
-    notifyOnError = true;
 
-    formattersByFt = {
-      bash = [
-          "shellcheck"
-          "shellharden"
-          "shfmt"
-      ];
-      nix = ["alejandra"];
-      markdown = [
-        [
-          "prettierd"
-          "prettier"
-        ]
-      ];
-    };
+    settings = {
+      notify_on_error = true;
 
-    formatters = {
-      shellcheck.command = "${lib.getExe pkgs.shellcheck}";
-      shellharden.command = "${lib.getExe pkgs.shellharden}";
-      shfmt.command = "${lib.getExe pkgs.shfmt}";
-      alejandra.command = "${lib.getExe pkgs.alejandra}";
-      prettierd.command = "${lib.getExe pkgs.prettierd}";
+      formatters = {
+        shellcheck.command = "${lib.getExe pkgs.shellcheck}";
+        shellharden.command = "${lib.getExe pkgs.shellharden}";
+        shfmt.command = "${lib.getExe pkgs.shfmt}";
+        alejandra.command = "${lib.getExe pkgs.alejandra}";
+        prettierd.command = "${lib.getExe pkgs.prettierd}";
+      };
+
+      formatters_by_ft = {
+        bash = [ "shellcheck" "shellharden" "shfmt" ];
+        nix = ["alejandra"];
+        markdown = [
+          [ "prettierd" "prettier" ]
+        ];
+      };
     };
   };
 }
--- config/plugins/ui/lualine.nix
+++ config/plugins/ui/lualine.nix
@@ -1,16 +1,20 @@
 _:{
   plugins.lualine = {
     enable = true;
-    globalstatus = true;
-    extensions = [
-      "fzf"
-      "neo-tree"
-    ];
 
-    disabledFiletypes = {
-      statusline = ["startup" "alpha"];
+    settings = {
+      globalstatus = true;
+
+      extensions = [
+        "fzf"
+        "neo-tree"
+      ];
+
+      disabledFiletypes = {
+        statusline = ["startup" "alpha"];
+      };
+
+      theme = "catppuccin";
     };
-
-    theme = "catppuccin";
   };
 }
